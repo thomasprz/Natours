b2e2f8d940271c7470169fcfef37129f42479fa9
Thomas <przegralek.thomas@gmail.com>
2025-05-04 21:46:44 +0200

    Modifications app.js

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ const app = express();
 // =======================
 // 1) MIDDLEWARES
 // =======================
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
