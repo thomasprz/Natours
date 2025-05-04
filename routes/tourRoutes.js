cb1c5cc7ae5578081b73c151a082bef45bf6fc0d
Thomas <przegralek.thomas@gmail.com>
2025-05-04 21:47:08 +0200

    Modifications tourRoutes.js

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
