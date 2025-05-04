304c24b558be9c0b93437de95c547581d380a6fd
Thomas <przegralek.thomas@gmail.com>
2025-05-04 21:46:52 +0200

    Modifications tourController.js

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,6 +16,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or Price',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -63,7 +73,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-
   res.status(200).json({
     status: 'success',
     data: {
