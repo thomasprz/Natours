a61150c31c7d0665507e8275937020a880f08f0d
Thomas <przegralek.thomas@gmail.com>
2025-05-05 22:24:41 +0200

    Update tour controller logic

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,40 +1,14 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`) //Ce fichier est seulement lu lorsque le serveur démarre. Une fois qu'on sauvegarde une modification dans tours-simple.json, cela va redémarrer le serveur
-);
-
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`); //Une fois la requête GET sur un tour réalisé , il va afficher dans le terminal VSC l'information Tour id is: 2
-  if (req.params.id * 1 > tours.length) {
-    // On convertit req.params.id en nombre (* 1 est un raccourci pour forcer le type).
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
-
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or Price',
-    });
-  }
-  next();
-};
+const Tour = require('./../models/tourModel');
 
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    //requestedAt: req.requestTime,
+    //results: tours.length,
+    //data: {
+    //  tours,
+    //},
   });
 };
 
@@ -42,34 +16,23 @@ exports.getTour = (req, res) => {
   //:id est un paramètre de route
   console.log(req.params);
   const id = req.params.id * 1; // * 1 car il va automatiquement convertir l'id en nombre tricks
-  const tour = tours.find((el) => el.id === id);
+  //const tour = tours.find((el) => el.id === id);
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  //res.status(200).json({
+  //  status: 'success',
+  //  data: {
+  //    tour,
+  //  },
+  //});
 };
 
 exports.createTour = (req, res) => {
-  console.log(req.body);
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/../dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
+  res.status(201).json({
+    status: 'success',
+    //data: {
+    //  tour: newTour,
+    //},
+  });
 };
 
 exports.updateTour = (req, res) => {
